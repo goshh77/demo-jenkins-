17b0bc0a8258ecfd9e03c6560a2bb5ef947b1ec2
harsha <hc20040621@gmail.com>
2026-06-12 18:58:23 +0530

    Added Kivo dashboard UI

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,25 +1,55 @@
 pipeline {
     agent any
 
+    environment {
+        PROJECT_ID = "harsha-498909"
+        REGION = "us-central1"
+        REPOSITORY = "jenkins-images"
+        IMAGE_NAME = "kivo-demo-app"
+        IMAGE_URI = "us-central1-docker.pkg.dev/harsha-498909/jenkins-images/kivo-demo-app:latest"
+    }
+
     stages {
 
         stage('Checkout') {
             steps {
-                git branch: 'main',
-                credentialsId: 'github-creds',
-                url: 'https://github.com/goshh77/demo-jenkins-.git'
+                checkout scm
             }
         }
 
         stage('Build Docker Image') {
             steps {
-                sh 'docker build -t kivo-demo-app .'
+                sh '''
+                docker build -t ${IMAGE_NAME}:latest .
+                '''
             }
         }
 
-        stage('Verify Image') {
+        stage('Tag Image') {
             steps {
-                sh 'docker images'
+                sh '''
+                docker tag ${IMAGE_NAME}:latest ${IMAGE_URI}
+                '''
+            }
+        }
+
+        stage('Push Image') {
+            steps {
+                sh '''
+                docker push ${IMAGE_URI}
+                '''
+            }
+        }
+
+        stage('Deploy Cloud Run') {
+            steps {
+                sh '''
+                gcloud run deploy ${IMAGE_NAME} \
+                --image ${IMAGE_URI} \
+                --region ${REGION} \
+                --platform managed \
+                --allow-unauthenticated
+                '''
             }
         }
     }
--- app.py
+++ app.py
@@ -1,15 +1,70 @@
 from flask import Flask
-import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return {
-        "application": "kivo-cicd-poc",
-        "status": "running",
-        "hostname": socket.gethostname()
-    }
+    return """
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>Kivo CI/CD Demo</title>
+        <style>
+            body {
+                font-family: Arial, sans-serif;
+                background: #0f172a;
+                color: white;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .card {
+                background: #1e293b;
+                padding: 40px;
+                border-radius: 16px;
+                box-shadow: 0 0 20px rgba(0,0,0,0.3);
+                text-align: center;
+                width: 500px;
+            }
+
+            .status {
+                color: #22c55e;
+                font-size: 24px;
+                font-weight: bold;
+            }
+
+            h1 {
+                margin-bottom: 20px;
+            }
+
+            .info {
+                margin-top: 20px;
+                font-size: 18px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="card">
+            <h1>🚀 Kivo CI/CD Pipeline Demo</h1>
+
+            <div class="status">
+                Application Running Successfully
+            </div>
+
+            <div class="info">
+                <p>Source Control: GitHub</p>
+                <p>CI/CD Tool: Jenkins</p>
+                <p>Container: Docker</p>
+                <p>Registry: Artifact Registry</p>
+                <p>Deployment: Cloud Run</p>
+            </div>
+        </div>
+    </body>
+    </html>
+    """
 
 @app.route("/health")
 def health():
